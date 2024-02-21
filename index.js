dd22fb4a0d9a92c91c7c814a1721ee2baeea34fa
Mwaimu <wizzbx@gmail.com>
2024-02-21 21:38:41 +0000

    pushing

--- index.js
+++ index.js
@@ -2,38 +2,63 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bodyParser = require('body-parser');
+const url = require('url')
+const { isUrl } = require('check-valid-url');
+
+const shortUniqueId = require('short-unique-id');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
+app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
+const customUid = new shortUniqueId({
+  length: 15, // Generate IDs of length 10
+  dictionary: 'alphanum', // Use only alphanumeric characters
+  encode: 'base64' // Encode using base64 for shorter, URL-safe IDs
+});
+app.post("/api/shorturl", (req, res) => {
 
-app.post("/api/shorturl",(req,res)=>{
-  
-  console.log(req.originalUrl)
+  console.log(req.body.url)
+  const ogURL = req.body.url;
+  const shortURL = customUid.rnd()
   let input = '', domain = '', param = '', short = 0;
 
-  input = req.body.url;
-  if (input === null || input === '') { 
-    return res.json({ error: 'invalid url' }); 
-  }
-  // console.log(req.)
+  isValidUrl(ogURL)
+    ? res.json({ original_url: ogURL, short_url: shortURL })
+    : res.json({ error: "invalid url" })
+
+  // res.json({original_url:ogURL,short_url:shortURL})
+
+
 
 })
 
+function isValidUrl(input) {
+
+  if (isUrl(input)) {
+    return true
+  }
+  else {
+    return false
+  }
+}
 
 
-app.listen(port, function() {
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
